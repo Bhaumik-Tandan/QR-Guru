d5a8a4a3a8121b73a631a8c467443f3eaeabcf34
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2023-10-12 16:05:33 +0530

    [ADD] basic icons and setup for qr scanner

--- navigator/TabNavigator.js
+++ navigator/TabNavigator.js
@@ -1,15 +1,41 @@
+import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import QRCodeScanner from '../QRCodeScanner';
-import QRCodeGenerator from '../QrCodeGenerator';
 import { NavigationContainer } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/FontAwesome'; // Replace 'FontAwesome' with the icon library of your choice
+import BannerAd from "../component/BannerAd";
+import QRCodeGenerator from '../screens/QrCodeGenerator';
+import QRCodeScanner from '../screens/QRCodeScanner';
+
 const Tab = createBottomTabNavigator();
 
 function TabNavigator() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Generator" component={QRCodeGenerator} />
-        <Tab.Screen name="Scanner" component={QRCodeScanner} />
+      <Tab.Navigator
+        screenOptions={{
+          header: () => (
+              <BannerAd /> 
+          ),
+        }}
+      >
+        <Tab.Screen
+          name="Scanner"
+          component={QRCodeScanner}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="camera" size={size} color={color} /> // Replace 'camera' with the icon name you want to use
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Generator"
+          component={QRCodeGenerator}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="qrcode" size={size} color={color} /> // Replace 'qrcode' with the icon name you want to use
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
