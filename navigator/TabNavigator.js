a43e4afb26569be757bca228076dd5707a6c9395
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2023-10-12 13:34:38 +0530

    [edit] SCREEN NAME

--- navigator/TabNavigator.js
+++ navigator/TabNavigator.js
@@ -8,8 +8,8 @@ function TabNavigator() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="QRCodeGenerator" component={QRCodeGenerator} />
-        <Tab.Screen name="QRCodeScanner" component={QRCodeScanner} />
+        <Tab.Screen name="Generator" component={QRCodeGenerator} />
+        <Tab.Screen name="Scanner" component={QRCodeScanner} />
       </Tab.Navigator>
     </NavigationContainer>
   );
