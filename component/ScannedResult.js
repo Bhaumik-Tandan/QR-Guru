4df9e3ff9d59af0dced7fd0bd03d2bd2cd816316
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2023-11-01 04:05:39 +0530

    [ADD] open link

--- component/ScannedResult.js
+++ component/ScannedResult.js
@@ -1,57 +1,68 @@
 import React from "react";
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  TextInput,
-  StyleSheet,
-} from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
 import { calcHeight, calcWidth, getFontSizeByWindowWidth } from "../helper/res";
-
+import * as Linking from 'expo-linking';
 const ScannedResult = ({ scannedData, handleScanAgain }) => {
+  const handleSearch = () => {
+    Linking.openURL(`https://www.google.com/search?q=${scannedData}`);
+  }
   return (
-    <View style={styles.outputContainer}>
-      <TextInput
-        value={scannedData}
-        style={styles.scannedText}
-        selectTextOnFocus={true}
-        multiline={true}
-        numberOfLines={10}
-        textAlignVertical={"center"}
-        textAlign="center"
-      />
-      <TouchableOpacity
-        style={styles.scanAgainButton}
-        onPress={handleScanAgain}
-      >
-        <Text selectable={true} style={styles.scanAgainText}>
-          Scan Again
-        </Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+      <Text style={styles.scannedOutput} selectable={true} selectionColor='orange'>
+        {scannedData}
+      </Text>
+      <View style={styles.buttonContainer}>
+        {renderIconButton("ios-scan-outline", "Scan Again", handleScanAgain)}
+        {renderIconButton("ios-copy-outline", "Copy", handleCopy)}
+        {renderIconButton("ios-search-sharp", "Search", handleSearch)}
+      </View>
     </View>
   );
 };
 
+const renderIconButton = (iconName, text, onPress) => (
+  <TouchableOpacity style={styles.button} onPress={onPress}>
+    <Ionicons name={iconName} size={24} color="black" />
+    <Text selectable={true} style={styles.buttonText}>
+      {text}
+    </Text>
+  </TouchableOpacity>
+);
+
+const handleCopy = () => {
+  // Implement your copy functionality here
+};
+
+const handleSearch = () => {
+  // Implement your search functionality here
+};
+
 const styles = StyleSheet.create({
-  outputContainer: {
+  container: {
     marginTop: 20,
     alignItems: "center",
   },
-  scanAgainButton: {
-    backgroundColor: "blue",
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginBottom: 20,
+  },
+  button: {
     padding: 10,
     borderRadius: 5,
+    alignItems: "center",
   },
-  scanAgainText: {
-    color: "white",
-  },
-  scannedText: {
+  buttonText: {
     color: "black",
-    width: calcWidth(80),
-    height: calcHeight(50),
-    backgroundColor: "white",
-    marginBottom: calcHeight(5),
+    marginTop: 5,
+  },
+  scannedOutput: {
+    margin: 50,
     fontSize: getFontSizeByWindowWidth(15),
+    padding: 20,
+    backgroundColor: "white",
+    width: calcWidth(80),
   },
 });
 
