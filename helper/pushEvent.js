8ebad00a19178261dcf4007eeb707e226fe3964f
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-05-17 10:58:36 +0530

    [upgrade] eas.json

--- helper/pushEvent.js
+++ helper/pushEvent.js
@@ -1,18 +1,4 @@
-import { ENV, API_URL } from "@env";
 function pushEvent(event) {
-  if (ENV !== "production") return;
-  console.log("ENV", ENV);
-
-  fetch(`${API_URL}/event`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ name: event }),
-  })
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err));
 }
 
 export default pushEvent;
