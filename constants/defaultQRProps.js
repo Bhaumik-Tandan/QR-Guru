12347de99ca981995a588cde7497f592777efd95
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-05-20 14:11:43 +0530

    [edit] new packge in editor

--- constants/defaultQRProps.js
+++ constants/defaultQRProps.js
@@ -1,11 +1,8 @@
 import { calcWidth } from "../helper/res";
 const defaultQRProps = {
   color: "#000000",
-  codeStyle: "square",
   backgroundColor: "#ffffff",
   logoSize: calcWidth(15),
-  outerEyeStyle: "square",
-  innerEyeStyle: "square",
   size:250
 };
 
